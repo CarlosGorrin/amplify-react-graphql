307e34c08f3a5389ec0177ddad09bb848a77b4b1
CarlosGorrin <carlos.gorrin@gmail.com>
2023-05-09 02:40:52 -0300

    improving choose a file input

--- src/App.js
+++ src/App.js
@@ -22,8 +22,9 @@ import {
 
 Amplify.configure(awsconfig);
 
-const App = ({ signOut }) => {
+const App = ({ signOut, selectedFile = null }) => {
   const [notes, setNotes] = useState([]);
+  const [selectedFileName, setSelectedFileName] = useState("");
 
   useEffect(() => {
     fetchNotes();
@@ -51,9 +52,11 @@ const App = ({ signOut }) => {
     const data = {
       name: form.get("name"),
       description: form.get("description"),
-      image: image.name,
+      image: selectedFile ? selectedFile.name : null,
     };
-    if (!!data.image) await Storage.put(data.name, image);
+      if (selectedFile) {
+        await Storage.put(data.name, selectedFile);
+      }
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -72,8 +75,19 @@ const App = ({ signOut }) => {
     });
   }
 
-  const capitalizeFirst = str => {
+  const capitalizeFirst = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  const hiddenFileInput = React.useRef(null);
+
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0];
+    setSelectedFileName(fileUploaded.name);
   };
 
   return (
@@ -97,50 +111,60 @@ const App = ({ signOut }) => {
             variation="quiet"
             required
           />
+          <Button onClick={handleClick}>
+            Upload a file
+          </Button>
           <View className="text-sm"
             name="image"
             as="input"
             type="file"
-            style={{ alignSelf: "end" }}
+            ref={hiddenFileInput}
+            onChange={handleChange}
+            style={{ alignSelf: "end", display:'none' }}
           />
+          {selectedFile && (
+            <Text as="span" className="text-gray-500 text-sm mb-4">
+              {selectedFile && <p>{selectedFile.name}</p>}
+            </Text>
+          )}
           <Button type="submit" variation="primary">
             Create Note
           </Button>
         </Flex>
       </View>
-      
-      <Heading level={2} className="text-2xl font-bold mb-4">Current Notes</Heading>
-      <View margin="3rem 1rem" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            className="bg-white p-4 rounded-md shadow-md"
-          >
-            {note.image && (
-               <Image className="relative w-full h-0 overflow-hidden aspect-ratio-square mb-4"
-               src={note.image}
-               alt={`visual aid for ${notes.name}`}
-               style={{ maxWidth: '350px', height: 'auto' }}
-             />
-            )}
-            <Text as="strong" fontWeight={700} className="text-xl mb-2">
-              {capitalizeFirst(note.name)}
-            </Text>
-            <Text as="span" className="text-gray-500 text-sm mb-4">{note.description}</Text>
-            <Button
-              variation="link"
-              onClick={() => deleteNote(note)}
-              className="text-red-500 hover:text-red-600 text-sm"
-            >
-              Delete note
-            </Button>
-          </Flex>
-        ))}
-      </View>
-      <Button margin="3rem 0" onClick={signOut}>Sign Out</Button>
+    <Heading level={2} className="text-2xl font-bold mb-4">Current Notes</Heading>
+    <View margin="3rem 1rem" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    {notes.map((note) => (
+    <Flex
+    key={note.id || note.name}
+    direction="column"
+    justifyContent="center"
+    alignItems="center"
+    className="bg-white p-4 rounded-md shadow-md"
+    >
+    {note.image && (
+    <Image
+    className="relative w-full h-0 overflow-hidden aspect-ratio-square mb-4"
+    src={note.image}
+    alt={`visual aid for ${notes.name}`}
+    style={{ maxWidth: '350px', height: 'auto' }}
+  />
+    )}
+    <Text as="strong" fontWeight={700} className="text-xl mb-2">
+    {capitalizeFirst(note.name)}
+    </Text>
+    <Text as="span" className="text-gray-500 text-sm mb-4">{note.description}</Text>
+    <Button
+    variation="link"
+    onClick={() => deleteNote(note)}
+    className="text-red-500 hover:text-red-600 text-sm"
+    >
+    Delete note
+    </Button>
+    </Flex>
+    ))}
+    </View>
+    <Button margin="3rem 0" onClick={signOut}>Sign Out</Button>
     </View>
   );
 };
